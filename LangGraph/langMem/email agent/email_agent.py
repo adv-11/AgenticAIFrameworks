3b960b073e654fdd93e76a855fcbb59042de5fc6
adv-11 <advaitss11@gmail.com>
2025-03-20 10:18:39 +0530

    email  bot LangMem

--- LangGraph/langMem/email agent/email_agent.py
+++ LangGraph/langMem/email agent/email_agent.py
@@ -64,3 +64,28 @@ llm_router = llm.with_structured_output(Router)
 print(triage_system_prompt)
 print(triage_user_prompt)
 
+system_prompt = triage_system_prompt.format(
+    full_name=profile["full_name"],
+    name=profile["name"],
+    examples=None,
+    user_profile_background=profile["user_profile_background"],
+    triage_no=prompt_instructions["triage_rules"]["ignore"],
+    triage_notify=prompt_instructions["triage_rules"]["notify"],
+    triage_email=prompt_instructions["triage_rules"]["respond"],
+)
+
+user_prompt = triage_user_prompt.format(
+    author=email["from"],
+    to=email["to"],
+    subject=email["subject"],
+    email_thread=email["body"],
+)
+
+result = llm_router.invoke(
+    [
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": user_prompt},
+    ]
+)
+
+print (result)
