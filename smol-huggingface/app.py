9521ef7948bccec0f9042de400365e3e9a86c653
adv-11 <advaitss11@gmail.com>
2025-04-12 11:51:46 +0530

    smol agents HF

---
+++ smol-huggingface/app.py
@@ -0,0 +1,8 @@
+from smolagents import CodeAgent, DuckDuckGoSearchTool, HfApiModel
+
+
+agent = CodeAgent(tools=[DuckDuckGoSearchTool()], model=HfApiModel())
+
+response = agent.run("How many seconds would it take for a leopard at full speed to run through Pont des Arts?")
+
+print (response)
